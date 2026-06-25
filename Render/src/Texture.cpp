e9a82803d2d512359937eeb262671fd4a66f426c
mozwb <1602368663@qq.com>
2026-06-25 18:10:40 +0800

    添加Texture

---
+++ Render/src/Texture.cpp
@@ -0,0 +1,38 @@
+﻿#include"Render/include/Texture.h"
+#include"Render/include/Render.h"
+#include<Render/include/OpenGL/OpenGLTexture.h>
+namespace X_Y {
+
+	Ref<Texture2D> Texture2D::Create(const TextureSpecification& specification)
+	{
+		RApiType Api = Render::instance()->getCurrentAPI()->getType();
+		switch (Api)
+		{
+		case RApiType::None:
+			XY_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+		case RApiType::OpenGL:
+			return CreateRef<OpenGLTexture2D>(specification);
+		}
+
+		XY_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+	Ref<Texture2D> Texture2D::Create(const std::string& path)
+	{
+		RApiType Api = Render::instance()->getCurrentAPI()->getType();
+		switch (Api)
+		{
+		case RApiType::None:
+			XY_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+		case RApiType::OpenGL:
+			return CreateRef<OpenGLTexture2D>(path);
+		}
+
+		XY_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+}
